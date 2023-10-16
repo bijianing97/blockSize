93879741870283df45ac96bc11d14fecdbba7021
bijianing97 <826015751@qq.com>
2023-10-16 14:58:06 +0800

    chore: add table

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import Web3 from "web3";
+import { table } from "table";
 const httpProvider = new Web3.providers.HttpProvider("http://127.0.0.1:42000");
 const web3 = new Web3(httpProvider);
 
 (async () => {
   const hardforkNumber = 15774234;
-  const intervalArray = [
-    10, 100, 1000, 10000, 20000, 30000, 50000, 80000, 100000,
+  const intervalArray = [100, 1000, 10000, 20000, 30000, 50000, 80000, 100000];
+  const tableData = [
+    ["BlockNumber", "totalSize/bytes", "totalSize/MB", "averageSize/bytes"],
   ];
-
   async function subsection(
     interval: number,
     forwardOrback: boolean
@@ -22,7 +23,7 @@ const web3 = new Web3(httpProvider);
         end = hardforkNumber + (i + 1) * minInterval;
       } else {
         start = hardforkNumber - (intervalNumber - i) * minInterval;
-        end = hardforkNumber - (intervalNumber - i + 1) * minInterval;
+        end = hardforkNumber - (intervalNumber - (i + 1)) * minInterval;
       }
       const sizeArray = [];
       for (let j = start; j < end; j++) {
@@ -42,8 +43,6 @@ const web3 = new Web3(httpProvider);
       return await getBlockSize(blockNumber);
     }
   }
-  const data = [["Interval", "totalSize", "averageSize"]];
-
   for (const interval of intervalArray) {
     const forwardSize = await subsection(interval, true);
     const backwardSize = await subsection(interval, false);
@@ -58,5 +57,19 @@ const web3 = new Web3(httpProvider);
         forwardSize / interval
       }`
     );
+    tableData.push([
+      `Before hardfork ${interval} blocks`,
+      backwardSize.toString(),
+      (backwardSize / 1024 / 1024).toString(),
+      (backwardSize / interval).toString(),
+    ]);
+    tableData.push([
+      `After hardfork ${interval} blocks`,
+      forwardSize.toString(),
+      (forwardSize / 1024 / 1024).toString(),
+      (forwardSize / interval).toString(),
+    ]);
+    console.log(table(tableData));
   }
+  console.log(table(tableData));
 })();
